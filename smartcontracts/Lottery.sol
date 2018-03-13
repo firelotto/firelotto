36ee365fa1e70dce3f80781a7bf40a1f742b38f8
polovaikin <polovaikin@gmail.com>
2018-03-13 20:02:25 +0300

    v1.2

--- smartcontracts/Lottery.sol
+++ smartcontracts/Lottery.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.20;
 
 contract Ownable {
     address public owner;
@@ -21,6 +21,9 @@ contract Ownable {
 
 
 contract Lottery is Ownable {
+
+    string constant version = "1.2";
+
     address public drawer;
 
     struct Game {
@@ -39,6 +42,7 @@ contract Lottery is Ownable {
         uint addToReserveAmount;
         uint bitcoinBlockIndex;
         string bitcoinBlockHash;
+        mapping(uint => uint) referalIdToTicketCount;
     }
 
     struct Ticket {
@@ -46,9 +50,14 @@ contract Lottery is Ownable {
         bytes numbers;
     }
 
+    struct Referal {
+        address wallet;
+        uint percent;
+    }
+
     mapping(address => uint[2][]) playerTickets;
 
-    mapping(address => bool) isBonusAddress;
+    Referal[][] public referals;
 
     Game[] public games;
 
@@ -74,19 +83,19 @@ contract Lottery is Ownable {
 
     address public technicalWallet;
 
-    address public prWallet;
-
     uint public dividendsPercent;
 
     uint public technicalPercent;
 
-    uint public prPercent;
+    uint public referalPercent;
 
     bool public buyEnable = true;
 
     uint public nextPrice;
 
     uint public intervalTime;
+
+    uint public percentDivider = 10000;
 
     modifier onlyDrawer() {
         require(msg.sender == drawer);
@@ -97,44 +106,58 @@ contract Lottery is Ownable {
     }
 
 
-    event LogTransfer(uint indexed gameIndex, uint riseAmount, uint technicalAmount, uint dividendsAmount, uint prAmount);
-
     event LogDraw(uint indexed gameIndex, uint startTime, uint bitcoinBlockIndex, bytes numbers, uint riseAmount, uint transferAmount, uint addToJackpotAmount, uint addToReserveAmount);
 
     event LogReserveUsed(uint indexed gameIndex, uint amount);
 
     function Lottery() public {
+        numbersCount = 4;
+
+        dividendsPercent = 1000;
+        technicalPercent = 500;
+        referalPercent = 1500;
 
         drawer = msg.sender;
         dividendsWallet = msg.sender;
         technicalWallet = msg.sender;
-        prWallet = msg.sender;
 
-        dividendsPercent = 10;
-        technicalPercent = 5;
-        prPercent = 15;
+        referals.length = 1;
+        referals[0].push(Referal(msg.sender, referalPercent));
 
         disableBuyingTime = 1 hours;
-        intervalTime = 6 hours;
+        intervalTime = 24 hours;
 
         nextPrice = 0.003 ether;
 
-        games.length += 2;
+        games.length = 2;
 
-        numbersCount = 6;
-        numbersCountMax = 45;
-        winPercent = [0, 0, 20, 20, 20, 20, 20];
+        if (numbersCount == 4) {
+            numbersCountMax = 20;
+            winPercent = [0, 0, 34, 33, 33];
 
-        jackpotGuaranteed = 1000 ether;
-        ticketCountMax = 1000000;
-        games[0].startTime = 1514872800;
+            jackpotGuaranteed = 3 ether;
+            ticketCountMax = 1000000;
+            games[0].startTime = 1520553600;
+        } else if (numbersCount == 5) {
+            numbersCountMax = 36;
+            winPercent = [0, 0, 25, 25, 25, 25];
+
+            jackpotGuaranteed = 143 ether;
+            ticketCountMax = 1000000;
+            games[0].startTime = 1520553600;
+        } else if (numbersCount == 6) {
+            numbersCountMax = 45;
+            winPercent = [0, 0, 20, 20, 20, 20, 20];
+
+            jackpotGuaranteed = 1430 ether;
+            ticketCountMax = 1000000;
+            games[0].startTime = 1520553600;
+        }
 
         games[0].price = nextPrice;
         games[1].price = nextPrice;
 
         games[1].startTime = games[0].startTime + intervalTime;
-
-
     }
 
     function startTime() public view returns (uint){
@@ -159,23 +182,46 @@ contract Lottery is Ownable {
     }
 
     function closeCurrentGame(uint bitcoinBlockIndex) public onlyDrawer {
-        if (isNeedCloseCurrentGame()) {
-            games[gameIndex].bitcoinBlockIndex = bitcoinBlockIndex;
-            gameIndexToBuy = gameIndex + 1;
-        }
+        require(isNeedCloseCurrentGame());
+
+        games[gameIndex].bitcoinBlockIndex = bitcoinBlockIndex;
+        gameIndexToBuy = gameIndex + 1;
     }
 
     function() public payable {
-        uint[] memory numbers = new uint [](msg.data.length);
+        uint refLength = msg.data.length % numbersCount;
+        uint[] memory numbers;
 
-        for (uint i = 0; i < msg.data.length; i++) {
-            numbers[i] = uint((msg.data[i] >> 4) & 0xF) * 10 + uint(msg.data[i] & 0xF);
+        if (refLength == 0) {
+            numbers = new uint [](msg.data.length);
+            for (uint i = 0; i < numbers.length; i++) {
+                numbers[i] = uint((msg.data[i] >> 4) & 0xF) * 10 + uint(msg.data[i] & 0xF);
+            }
+            buyTicket(numbers, 0);
+        } else {
+            uint referalId;
+            if (refLength == 1) {
+                referalId = uint(msg.data[msg.data.length - 1]);
+            } else if (refLength == 2) {
+                referalId = (uint(msg.data[msg.data.length - 2]) << 8) + uint(msg.data[msg.data.length - 1]);
+            }
+
+            numbers = new uint [](msg.data.length - refLength);
+
+            uint shift = referalId % 10;
+            if (shift == 0) shift = 1;
+
+            for (i = 0; i < numbers.length; i++) {
+                numbers[i] = uint(msg.data[i]) - shift;
+            }
+
+            buyTicket(numbers, referalId);
         }
-        buyTicket(numbers, address(0));
     }
 
-    function buyTicket(uint[] numbers, address bonusAddress) public payable {
+    function buyTicket(uint[] numbers, uint referalId) public payable {
         require(buyEnable);
+        require(referalId < referals.length);
         require(numbers.length % numbersCount == 0);
 
         Game storage game = games[gameIndexToBuy];
@@ -200,12 +246,7 @@ contract Lottery is Ownable {
             game.tickets.push(Ticket(msg.sender, bet));
 
         }
-
-        if (isBonusAddress[bonusAddress]) {
-            bonusAddress.transfer(msg.value * prPercent / 100);
-        } else {
-            prWallet.transfer(msg.value * prPercent / 100);
-        }
+        game.referalIdToTicketCount[referalId] += buyTicketCount;
     }
 
     function getPlayerTickets(address player, uint offset, uint count) public view returns (int [] tickets){
@@ -219,7 +260,7 @@ contract Lottery is Ownable {
         tickets = new int []((n - offset) * (numbersCount + 5));
 
         for (uint i = offset; i < n; i++) {
-            var info = list[list.length - i - 1];
+            uint[2] storage info = list[list.length - i - 1];
             uint gameIndex = info[0];
 
             tickets[k++] = int(gameIndex);
@@ -245,6 +286,51 @@ contract Lottery is Ownable {
                     }
                     else {
                         tickets[k++] = int(games[gameIndex].tickets[info[1]].numbers[j]);
+                    }
+                }
+            }
+        }
+    }
+
+    function getAllTickets() public view returns (int [] tickets){
+        uint n = gameIndexToBuy + 1;
+
+        uint ticketCount;
+        for (uint gameIndex = 0; gameIndex < n; gameIndex++) {
+            ticketCount += games[gameIndex].tickets.length;
+        }
+
+        tickets = new int[](ticketCount * (numbersCount + 5));
+        uint k;
+
+        for (gameIndex = 0; gameIndex < n; gameIndex++) {
+            Ticket[] storage gameTickets = games[gameIndex].tickets;
+            for (uint ticketIndex = 0; ticketIndex < gameTickets.length; ticketIndex++) {
+
+                tickets[k++] = int(gameIndex);
+                tickets[k++] = int(ticketIndex);
+                tickets[k++] = int(games[gameIndex].startTime);
+
+                if (games[gameIndex].winNumbers.length == 0) {
+                    tickets[k++] = - 1;
+                    tickets[k++] = int(games[gameIndex].price);
+
+                    for (uint j = 0; j < numbersCount; j++) {
+                        tickets[k++] = int(games[gameIndex].tickets[ticketIndex].numbers[j]);
+                    }
+                }
+                else {
+                    uint winNumbersCount = getEqualCount(games[gameIndex].tickets[ticketIndex].numbers, games[gameIndex]);
+                    tickets[k++] = int(games[gameIndex].winLevelAmounts[winNumbersCount]);
+                    tickets[k++] = int(games[gameIndex].price);
+
+                    for (j = 0; j < numbersCount; j++) {
+                        if (games[gameIndex].winNumbersMap[games[gameIndex].tickets[ticketIndex].numbers[j]]) {
+                            tickets[k++] = - int(games[gameIndex].tickets[ticketIndex].numbers[j]);
+                        }
+                        else {
+                            tickets[k++] = int(games[gameIndex].tickets[ticketIndex].numbers[j]);
+                        }
                     }
                 }
             }
@@ -341,7 +427,7 @@ contract Lottery is Ownable {
         for (i = 0; i < numbersCount; i++) {
             uint n = numbersCountMax - i;
 
-            uint r = uint(random[random.length - 1 - i]) % n;
+            uint r = (uint(random[i * 4]) + (uint(random[i * 4 + 1]) << 8) + (uint(random[i * 4 + 2]) << 16) + (uint(random[i * 4 + 3]) << 24)) % n;
 
             winNumbers[i] = allNumbers[r];
 
@@ -374,14 +460,20 @@ contract Lottery is Ownable {
 
         uint riseAmount = game.tickets.length * game.price;
 
-        uint technicalAmount = riseAmount * technicalPercent / 100;
-        uint dividendsAmount = riseAmount * dividendsPercent / 100;
-        uint prAmount = riseAmount * prPercent / 100;
+        uint technicalAmount = riseAmount * technicalPercent / percentDivider;
+        uint dividendsAmount = riseAmount * dividendsPercent / percentDivider;
 
         technicalWallet.transfer(technicalAmount);
         dividendsWallet.transfer(dividendsAmount);
 
-        LogTransfer(gameIndex, riseAmount, technicalAmount, dividendsAmount, prAmount);
+        for (uint referalId = 0; referalId < referals.length; referalId++) {
+            Referal[] storage refList = referals[referalId];
+            riseAmount = game.referalIdToTicketCount[referalId] * game.price;
+            for (i = 0; i < refList.length; i++) {
+                Referal storage ref = refList[i];
+                ref.wallet.transfer(riseAmount * ref.percent / percentDivider);
+            }
+        }
 
         games.length++;
 
@@ -394,7 +486,7 @@ contract Lottery is Ownable {
     function calcWins(Game storage game) private {
         game.checkWinTicketLevel = numbersCount;
 
-        uint riseAmount = game.tickets.length * game.price * (100 - technicalPercent - dividendsPercent - prPercent) / 100;
+        uint riseAmount = game.tickets.length * game.price * (percentDivider - technicalPercent - dividendsPercent - referalPercent) / percentDivider;
 
         uint freeAmount = 0;
 
